dca15f2bfd4c32bcd588637d5dacd72303820b31
qkrrhksdlr <antonio2929@naver.com>
2021-03-29 16:44:51 +0900

    Delete Stitching directory

--- Stitching/solution.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-cv::Mat findHomography(cv::Mat& refImage, cv::Mat& queryImage, int distThresh);
-void warping(cv::Mat& queryImage, cv::Mat& I_image, cv::Mat& H, int width, int height);
-void blending(cv::Mat& refImage, cv::Mat& queryImage, cv::Mat& I_image, cv::Mat& result, int overlapWidth, int x_point, int y_point);
